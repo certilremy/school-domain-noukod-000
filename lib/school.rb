0fec7134babaad64018874e6a58b932168e48c39
Certil Remy <certil@noukod.com>
2018-10-10 20:11:35 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,17 +1,18 @@
-
-
-code here!
-
-class School
-
-
-  def initialized(name)
-    @name=name
-
-  end
-
-  def name
-    @name
-end
-end
 school = School.new("Bayside High School")
+
+
+
+# code here!
+
+#class School
+
+
+  #def initialized(name)
+    #@name=name
+
+  #end
+
+  #def name
+    #@name
+#  end
+#end
